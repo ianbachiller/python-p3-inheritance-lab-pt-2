d0adadd298a1fe7948b2a2bca8cd7d013bc113f3
Ian Bachiller <ianbachise@gmail.com>
2024-05-09 03:47:17 -0400

    Completed unheritance lab part 2

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,22 @@
+# Define two classes, one of which inherits from another.
+# Define methods in the child class that use the super keyword to inherit from and augment methods in the parent class.
+
+
 class Student:
-    pass
+    def __init__(self, greeting="Hey there! I'm so excited to learn stuff."):
+        self.greeting = greeting
+
+    def hello(self):
+        print(self.greeting)
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()  
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()
